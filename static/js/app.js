1ddb44c92920957abe2f9d329439569f9c28738a
Star Rota <Star@Stars-Air.fios-router.home>
2020-06-17 19:09:40 -0400

    working app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,14 @@
 function populateDropDown() {
   distinctList=[]
-  fetch('https://pridetrip.herokuapp.com/zones').then(data=>data.json()).then(d=>{
-  d.results.forEach(n=>{
-      o = document.createElement("option")
-      o.text = n.dozone
-      document.getElementById('selDataset').add(o)
-  })
+  fetch('https://pridetrip.herokuapp.com/zones').then(data=>data.json().then(d=>{
+    d.results.forEach(n=>{
+        o = document.createElement("option")
+        o.text = n.dozone
+        document.getElementById('selDataset').add(o)
+    })
+  }))
+}
 
-})}
-​
 function populateTable(dozone) {
   var pannel = d3.select("#trip-metadata")
   document.getElementById("trip-metadata").innerHTML = ""
@@ -17,26 +17,14 @@ function populateTable(dozone) {
   tip_amount = []
   payment_type = []
   trip_distance=[]
-​
+  
   fetch('https://pridetrip.herokuapp.com/pridedata?length=10000').then(data=>data.json()).then(d=>{
   d.results.forEach(r=>{
       if (dozone == r.dozone || typeof dozone == "undefined") {
           passenger_count.push(r.passenger_count)
-      }
-​
-      if (dozone == r.dozone || typeof dozone == "undefined") {
           total_amount.push(r.total_amount)
-      }
-     
-      if (dozone == r.dozone || typeof dozone == "undefined") {
           tip_amount.push(r.tip_amount)
-      }
-​
-      if (dozone == r.dozone || typeof dozone == "undefined") {
           payment_type.push(r.payment_type)
-      }
-​
-      if (dozone == r.dozone || typeof dozone == "undefined") {
           trip_distance.push(r.trip_distance)
       }
   })
@@ -45,8 +33,7 @@ function populateTable(dozone) {
   tip = jStat.mean(tip_amount)
   payment = jStat.mean(payment_type)
   distance = jStat.mean(trip_distance)
-​
-  
+
   var type;
   switch (payment) {
       case 1:
@@ -67,29 +54,27 @@ function populateTable(dozone) {
       case 6:
         type = "Voided Trip";
     }
-​
+    
   pannel.append("h4").text("Passenger Count: " + Math.round(passenger))   
   pannel.append("h4").text("Trip Cost: $" + Math.round(total))
   pannel.append("h4").text("Tip: $" + Math.round(tip))
   pannel.append("h4").text("Payment Type: " + type)
   pannel.append("h4").text("Trip Distance: " + Math.round(distance) + " mi" )
-​
+
 })
 }
 
-​
+
 populateTable()
 populateDropDown()
 
-​
-​
-​
-​
+
+
+
 function optionChanged(dropoffzone) {
   console.log(dropoffzone);
   populateTable(dropoffzone)
 
-​
 }
 
 
